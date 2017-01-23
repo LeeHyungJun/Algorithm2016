c3310d68a4caab6060ca5de76eb241dbb230442f
Hyungjun Lee <hjlee1765@gmail.com>
2017-01-23 18:19:10 +0900

    Create Binary_Search_Tree.cpp

---
+++ Binary_Search_Tree.cpp
@@ -0,0 +1,75 @@
+/**
+*	file   : Day22_BST.cpp
+*	date   : 01.23.2017
+*	author : Hyungjun Lee
+*	mail   : hjlee1765@gmail.com
+*	refer  : Hacker Rank
+**/
+
+#include <iostream>
+#include <cstddef>
+
+using namespace std;
+
+class Node {
+public:
+	int data;
+	Node *left;
+	Node *right;
+	Node(int d) {
+		data = d;
+		left = NULL;
+		right = NULL;
+	}
+};
+class Solution {
+public:
+	Node* insert(Node* root, int data) {
+		if (root == NULL) {
+			return new Node(data);
+		}
+		else {
+			Node* cur;
+			if (data <= root->data) {
+				cur = insert(root->left, data);
+				root->left = cur;
+			}
+			else {
+				cur = insert(root->right, data);
+				root->right = cur;
+			}
+
+			return root;
+		}
+	}
+	int getHeight(Node* root) {
+		if (root == NULL)
+			return -1;
+
+		int left = getHeight(root->left);
+		int right = getHeight(root->right);
+
+		if (left > right)
+			return left + 1;
+		else
+			return right + 1;
+	}
+};
+
+int main() {
+	Solution myTree;
+	Node* root = NULL;
+	int t;
+	int data;
+
+	cin >> t;
+
+	while (t-- > 0) {
+		cin >> data;
+		root = myTree.insert(root, data);
+	}
+	int height = myTree.getHeight(root);
+	cout << height;
+
+	return 0;
+}
