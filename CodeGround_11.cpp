5d30e3f6cac208ee466c6a115907fcc619e32f2f
Hyungjun Lee <hjlee1765@gmail.com>
2017-02-19 18:38:01 +0900

    Greedy Algorithm

--- CodeGround_11.cpp
+++ CodeGround_11.cpp
@@ -10,12 +10,7 @@ using namespace std;
 vector<int> v;
 
 int main(int argc, char** argv) {
-	/* 아래 freopen 함수는 input.txt 를 read only 형식으로 연 후,
-	앞으로 표준 입력(키보드) 대신 input.txt 파일로 부터 읽어오겠다는 의미의 코드입니다.
-	만약 본인의 PC 에서 테스트 할 때는, 입력값을 input.txt에 저장한 후 freopen 함수를 사용하면,
-	그 아래에서 scanf 함수 또는 cin을 사용하여 표준입력 대신 input.txt 파일로 부터 입력값을 읽어 올 수 있습니다.
-	또한, 본인 PC에서 freopen 함수를 사용하지 않고 표준입력을 사용하여 테스트하셔도 무방합니다.
-	단, Codeground 시스템에서 "제출하기" 할 때에는 반드시 freopen 함수를 지우거나 주석(//) 처리 하셔야 합니다. */
+	
 	freopen("input.txt", "r", stdin);
 
 	setbuf(stdout, NULL);
