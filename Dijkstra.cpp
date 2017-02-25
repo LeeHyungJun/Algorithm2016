d7aee874c72c0f1fee442762de174b2a69902ba0
Hyungjun Lee <hjlee1765@gmail.com>
2017-02-25 19:26:30 +0900

    다익스트라 && 2차원 벡터 동적할당 && 페어정렬 && 우선순위큐

--- Dijkstra.cpp
+++ Dijkstra.cpp
@@ -1,5 +1,6 @@
-//다익스트라 using priority queue.
 
+//다익스트라 using priority queue.
+//방향그래프, 서로 다른 두 정점 사이에 여러개 간선이 존재 가능.
 
 #include <iostream>
 #include <stdio.h>
@@ -10,25 +11,67 @@
 #include <functional>	//greater, less
 using namespace std;
 
-#define V 200001
-#define E 300001
+#define V 20001
+typedef pair<int, int> tpair;
 
-vector<vector<pair<int,int>>> adj(V);
-vector<int> d;
-priority_queue<int,vector<int>,greater<int>> q;
+//연산자 오버로딩
+/*
+struct cmp {
+	bool operator()(tpair a, tpair b) {
+		if (a.second == b.second)
+			return a.first < b.first;
+		return a.second < b.second;
+	}
+};*/
 
 
 int main(void) {
 	int n, m, s;
 	scanf("%d %d", &n, &m);
 	scanf("%d", &s);
-	for (int i = 0; i < m; i++) {
+
+	//adj : 노드 n에 인접한 노드를 찾는 2차원 벡터(동적할당)
+	vector<vector<tpair>> adj(n+1);
+	//d값이 가장 낮은 노드를 추출하기 위한 우선순위 큐
+	//q.first : d값, q.second : 노드번호 
+	//이 상태로 pop시, first 오름차순을 기준으로 정렬.
+	priority_queue<tpair, vector<tpair>, greater<tpair>> q;
+
+
+	for (int i = 1; i <= m; i++) {
 		int u, v, w;
 		scanf("%d %d %d", &u, &v, &w);
-		adj[u].push_back(pair<int, int>(v, w));
+		//동적할당.
+		adj[u].push_back(pair<int,int>(v,w));
 	}
-	//initialize
-	
 
+	vector<int> d(n + 1, 1000000);
+	d[s] = 0;
+	//q.first : d값 , q.second : 노드번호
+	q.push(pair<int, int>(d[s],s));
 
+	while (!q.empty()) {
+		int curNode = q.top().second;
+		int cost = q.top().first;
+		q.pop();
+		for (int i = 0; i < adj[curNode].size(); i++) {
+			int adjNode = adj[curNode][i].first;
+			int weight = adj[curNode][i].second;
+
+			if (d[adjNode] > d[curNode] + weight) {
+				d[adjNode] = d[curNode] + weight;
+				q.push(pair<int, int>(d[adjNode], adjNode));
+			}
+			//이 문장 여기다 놨다가 메모리초과 존나..ㅠㅜ
+			//q.push(pair<int, int>(d[adjNode], adjNode));
+		}
+	}
+
+	for (int i = 1; i <= n; i++) {
+		if (d[i] == 1000000)
+			printf("INF\n");
+		else
+			printf("%d\n", d[i]);
+	}
+	return 0;
 }
