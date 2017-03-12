8a7ad8e53eb2bcc50e68d1364004490b57e928df
Hyungjun Lee <hjlee1765@gmail.com>
2017-03-12 18:46:31 +0900

    my_strlen, strcat, 주소값 이동

--- my_strcat_strlen.c
+++ my_strcat_strlen.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
-#include <string.h>
-#include <cstring>
+
+int my_strlen(char* str) {
+	int len = 0;
+	while (*str) {
+		len++;
+		str++;
+	}
+	return len;
+}
+
+
 void my_strcat(char* str1, char* str2) {
-	int len = strlen(str1);
+	int len = my_strlen(str1);
 	while (*str2) {
 		*(str1 + len++) = *str2;
 		//주소값이동, index를 다음으로 하는 것과 같은 효과
